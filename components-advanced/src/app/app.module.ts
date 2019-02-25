48ba1b83c52be2d6fefa4ee5fd50093ff8af9ced
habeebcycle <guessy2k2@yahoo.com>
2019-02-25 13:09:11 +1100

    Tile pos

--- components-advanced/src/app/app.module.ts
+++ components-advanced/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostTileComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule
---
+++ components-advanced/src/app/blog-post-tile/blog-post-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-tile',
+  templateUrl: './blog-post-tile.component.html',
+  styleUrls: ['./blog-post-tile.component.scss']
+})
+export class BlogPostTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
